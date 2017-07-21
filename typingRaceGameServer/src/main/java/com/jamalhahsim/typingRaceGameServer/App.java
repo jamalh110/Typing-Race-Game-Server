df728d1026e12c92bbba7c738c8172e00e663893
jhashim <jamalh110@gmail.com>
2017-07-21 13:30:51 -0400

    first push

---
+++ typingRaceGameServer/src/main/java/com/jamalhahsim/typingRaceGameServer/App.java
@@ -0,0 +1,13 @@
+package com.jamalhahsim.typingRaceGameServer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
